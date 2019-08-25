5c9339e6d91625859b0fd535d9b1288299d48d56
Jameson Merkow <jmerkow@gmail.com>
2019-08-25 16:42:34 -0700

    add soft thresholding, fix for extra scalars

--- segmentation_models/encoder_decoder/segmentation.py
+++ segmentation_models/encoder_decoder/segmentation.py
@@ -64,7 +64,7 @@ class SegmentationModel(EncoderDecoder):
         mask = self.decoder(features)
         if self.encoder_classify:
             scalars = {k: args[k] for k in self.required_inputs}
-            score = self.encoder_classifier(features[0], **args)
+            score = self.encoder_classifier(features[0], **scalars)
             return [mask, score]
         return mask
 
@@ -171,99 +171,3 @@ def get_model(base, **model_params):
         preprocessing = get_preprocessing_fn(model_params['encoder'], pretrained=encoder_weights)
 
     return model, preprocessing
-
-# def get_segmententation_model(encoder_name='resnet34', decoder_name=None, decoder_kwargs=None,
-#                               activation='sigmoid',
-#                               encoder_weights="imagenet", name=None, classes=1):
-#     class SegModel(EncoderDecoder):
-#         def __init__(
-#                 self,
-#                 encoder_name,
-#                 decoder_name,
-#                 activation='sigmoid',
-#                 encoder_weights="imagenet",
-#                 name=None,
-#                 decoder_kwargs=None,
-#                 classes=1,
-#
-#         ):
-#             assert decoder_name is not None, "must specify decoder"
-#             decoder_kwargs = decoder_kwargs or {}
-#
-#             encoder = get_encoder(
-#                 encoder_name,
-#                 encoder_weights=encoder_weights
-#             )
-#
-#             decoder_cls = get_decoder_cls(decoder_name)
-#
-#             decoder = decoder_cls(
-#
-#                 **decoder_kwargs
-#             )
-#
-#             self.classes = classes
-#             super().__init__(encoder, decoder, activation)
-#
-#             if name is None:
-#                 name = '{}-{}-{}'.format(decoder_name.lower(), encoder_name, activation)
-#             self.name = name
-#
-#     return SegModel(encoder_name, decoder_name, activation=activation,
-#                     encoder_weights=encoder_weights,
-#                     name=name, classes=classes,
-#                     decoder_kwargs=decoder_kwargs)
-#
-#
-# def UNet(**kwargs):
-#     decoder_kwargs = dict(
-#         use_batchnorm=True,
-#         decoder_channels=(256, 128, 64, 32, 16),
-#         center=False)
-#     decoder_kwargs.update(kwargs.pop('decoder_kwargs', {}) or {})
-#     kwargs['decoder_name'] = 'UNET'
-#     kwargs['decoder_kwargs'] = decoder_kwargs
-#     kwargs.setdefault('activation', 'sigmoid')
-#
-#     return get_segmententation_model(**kwargs)
-#
-#
-# def PSPNet(**kwargs):
-#     decoder_kwargs = dict(
-#         downsample_factor=8,
-#         psp_out_channels=512,
-#         use_batchnorm=True,
-#         aux_output=False,
-#         dropout=0.2, )
-#     decoder_kwargs.update(kwargs.pop('decoder_kwargs', {}) or {})
-#     kwargs['decoder_name'] = 'PSP'
-#     kwargs['decoder_kwargs'] = decoder_kwargs
-#     kwargs.setdefault('activation', 'sigmoid')
-#
-#     return get_segmententation_model(**kwargs)
-#
-#
-# def LinkNet(**kwargs):
-#     decoder_kwargs = dict(
-#         prefinal_channels=32,
-#     )
-#     decoder_kwargs.update(kwargs.pop('decoder_kwargs', {}) or {})
-#     kwargs['decoder_name'] = 'LINK'
-#     kwargs['decoder_kwargs'] = decoder_kwargs
-#     kwargs.setdefault('activation', 'sigmoid')
-#
-#     return get_segmententation_model(**kwargs)
-#
-#
-# def FPN(**kwargs):
-#     decoder_kwargs = dict(
-#             pyramid_channels=256,
-#             segmentation_channels=128,
-#             dropout=0.2
-#     )
-#     decoder_kwargs.update(kwargs.pop('decoder_kwargs', {}) or {})
-#     kwargs['decoder_name'] = 'FPN'
-#     kwargs['decoder_kwargs'] = decoder_kwargs
-#     kwargs.setdefault('activation', 'sigmoid')
-#
-#     return get_segmententation_model(**kwargs)
